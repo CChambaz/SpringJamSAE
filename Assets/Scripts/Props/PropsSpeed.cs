e50d578b81695f102f2e49496c0cad404b401986
Sebastien Feser <sedukafe@gmail.com>
2020-06-19 21:20:54 +0200

    created basic box and metalBeam

---
+++ Assets/Scripts/Props/PropsSpeed.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PropsSpeed : MonoBehaviour
+{
+    public float zSpeed = 3;
+    [SerializeField] private Rigidbody propRigidbody;
+    void Update()
+    {
+        propRigidbody.velocity = new Vector3(propRigidbody.velocity.x, propRigidbody.velocity.y, -zSpeed);
+    }
+}
