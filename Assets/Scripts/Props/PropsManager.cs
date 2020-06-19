1ff6f9f1cd486ce7ad6ec7192c0fe49cec579e01
Sebastien Feser <sedukafe@gmail.com>
2020-06-19 22:08:54 +0200

    created basic spawner

--- Assets/Scripts/Props/PropsManager.cs
+++ Assets/Scripts/Props/PropsManager.cs
@@ -6,15 +6,36 @@ public class PropsManager : MonoBehaviour
 {
 
     public float propsSpeed = 3f;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+
+    [SerializeField] private Vector3 spawnPosition;
+
+    [SerializeField] private float spawnXMin;
+    [SerializeField] private float spawnXMax;
+
+    [SerializeField] private GameObject woodenBox;
+    [SerializeField] private GameObject metalBeam;
+
+    [SerializeField] private float spawnTime;
+    private float currentSpawnTime = 0;
+
 
     // Update is called once per frame
     void Update()
     {
-        
+        currentSpawnTime += Time.deltaTime;
+        if (currentSpawnTime >= spawnTime)
+        {
+            float random = Random.Range(0, 2);
+            if (random == 0)
+            {
+                Instantiate(woodenBox, new Vector3(Random.Range(spawnXMin, spawnXMax), spawnPosition.y, spawnPosition.z), Quaternion.identity);
+            }
+            else
+            {
+                Instantiate(metalBeam, new Vector3(Random.Range(spawnXMin, spawnXMax), spawnPosition.y, spawnPosition.z), Quaternion.identity);
+            }
+
+            currentSpawnTime = 0;
+        }
     }
 }
--- Assets/Scripts/Props/PropsSpeed.cs
+++ Assets/Scripts/Props/PropsSpeed.cs
@@ -6,8 +6,14 @@ public class PropsSpeed : MonoBehaviour
 {
     public float zSpeed = 3;
     [SerializeField] private Rigidbody propRigidbody;
+    [SerializeField] private float deleteY = 10f;
     void Update()
     {
         propRigidbody.velocity = new Vector3(propRigidbody.velocity.x, propRigidbody.velocity.y, -zSpeed);
+
+        if (transform.position.y < -10f)
+        {
+            Destroy(gameObject);
+        }
     }
 }
