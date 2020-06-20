0d5f74d323d498036da4c62736a8dad3dddc77b5
Sebastien Feser <sedukafe@gmail.com>
2020-06-20 19:09:44 +0200

    added Pattern2

---
+++ Assets/Scripts/SpawnableAreas.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnableAreas : MonoBehaviour
+{
+    //[SerializeField] private float spawnRateHurricane;
+    //[SerializeField] private float spawnRateCollectible;
+    [SerializeField] [Range(0f, 1f)] private float spawnRate;
+    [SerializeField] [Range(0f, 1f)] private float spawnRateHurricanesForCollectibles;
+
+
+    [SerializeField] private GameObject collectible;
+    [SerializeField] private GameObject hurricane;
+
+    private void Start()
+    {
+        float random = Random.Range(0f, 1f);
+
+        if (random < spawnRate)
+        {
+            float random2 = Random.Range(0f, 1f);
+            if (random2 < spawnRateHurricanesForCollectibles)
+            {
+                Instantiate(collectible, transform.position, Quaternion.identity);
+            }
+            else
+            {
+                Instantiate(hurricane, transform.position, Quaternion.identity);
+            }
+        }
+
+
+    }
+
+}
