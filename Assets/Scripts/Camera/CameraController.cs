944998dbb0c09d9ebf9c0331fa81ceca6b2a3ac1
Sandwish <dylan_vonarx@hotmail.fr>
2020-06-20 16:46:11 +0200

    Fixed bug Camera controller

--- Assets/Scripts/Camera/CameraController.cs
+++ Assets/Scripts/Camera/CameraController.cs
@@ -8,6 +8,7 @@ public class CameraController : MonoBehaviour
     [SerializeField] private Vector3 positionOffset;
     [SerializeField] private Quaternion rotationOffset;
     private Transform target;
+    private PlayerController player;
 
     [Header("Shaking effect")]
     [SerializeField] private bool isReady = true;
@@ -18,8 +19,9 @@ public class CameraController : MonoBehaviour
     [SerializeField] private float distanceShake;
 
     //params Effect
-    private const float Z_ROT_1 = 0.1f;
-    private const float MAX_Y_1 = 0.8f;
+    private const float Z_ROT = 0.15f;
+    private const float MAX_Y = 0.8f;
+    private const float MAX_Z = 5;
 
     private void ShakingEffect(float power = 1.0f)
     {
@@ -39,9 +41,10 @@ public class CameraController : MonoBehaviour
 
         if (target == null)
         {
-            if (GameObject.Find("Player(Clone)") != null)
+            if (GameObject.Find("Ch17_nonPBR") != null)
             {
-                target = GameObject.Find("Player(Clone)").transform;
+                target = GameObject.Find("Ch17_nonPBR").transform;
+                player = target.gameObject.GetComponent<PlayerController>();
                 transform.position = target.position + positionOffset;
                 transform.rotation = rotationOffset;
 
@@ -49,20 +52,20 @@ public class CameraController : MonoBehaviour
         }
         else
         {
-            if (!target.gameObject.GetComponent<PlayerController>().isDead)
+            if (!player.isDead)
             {
                 Vector3 newPosition = new Vector3(target.position.x, target.position.y);
                 float power = 1.0f;
 
-                if (target.position.z <= 6)
+                if (target.position.z <= MAX_Z)
                 {
                     power = 4.5f;
-                    if (transform.rotation.x <= Z_ROT_1)
+                    if (transform.rotation.x <= Z_ROT)
                     {
                         transform.Rotate(Vector3.right, 1.5f);
                     }
 
-                    if (positionYBord > MAX_Y_1)
+                    if (positionYBord > MAX_Y)
                     {
                         positionYBord += -1 * Time.deltaTime * 2.5f;
                     }
@@ -73,7 +76,7 @@ public class CameraController : MonoBehaviour
                 else
                 {
                     float velocity_z = target.gameObject.GetComponent<Rigidbody>().velocity.z;
-                    if (positionZBord <= 6 && velocity_z > 0)
+                    if (positionZBord <= MAX_Z && velocity_z > 0)
                     {
                         positionZBord += Time.deltaTime * 2.0f;
                     }
@@ -85,7 +88,7 @@ public class CameraController : MonoBehaviour
 
                     if (transform.rotation.x > 0.0f)
                     {
-                        transform.Rotate(Vector3.left, 0.2f);
+                        transform.Rotate(Vector3.left, 0.5f);
                     }
 
                     newPosition += new Vector3(0, positionYBord, target.position.z + positionZBord) + positionOffset;
