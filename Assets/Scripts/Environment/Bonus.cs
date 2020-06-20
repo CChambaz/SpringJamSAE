fc6df9da015d901616f1aa64d87126e89b056680
CChambaz <cdricchambaz@yahoo.fr>
2020-06-21 00:14:45 +0200

    Added model and the collectible elements now randomly choose between multiple models

--- Assets/Scripts/Environment/Bonus.cs
+++ Assets/Scripts/Environment/Bonus.cs
@@ -1,6 +1,8 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class Bonus : MonoBehaviour
 {
@@ -11,4 +13,18 @@ public class Bonus : MonoBehaviour
     }
 
     [SerializeField] public BonusType bonusType;
+    [SerializeField] private GameObject[] objectiveModels;
+    [SerializeField] private Vector3[] objectiveModelRotations;
+
+    private void Start()
+    {
+        if (bonusType == BonusType.HURRICAN_BEER)
+            return;
+        
+        RotatingObject rotatingObject = GetComponentInChildren<RotatingObject>();
+
+        int id = Random.Range(0, objectiveModels.Length);
+        Instantiate(objectiveModels[id], Vector3.zero, Quaternion.Euler(objectiveModelRotations[id]),
+            rotatingObject.transform).transform.localPosition = Vector3.zero;
+    }
 }
