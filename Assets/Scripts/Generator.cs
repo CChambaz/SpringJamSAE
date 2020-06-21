fa238bebca447d8834a62c31f9419e87a1016cb3
Sandwish <dylan_vonarx@hotmail.fr>
2020-06-21 15:38:52 +0200

    Added Animation Generator

--- Assets/Scripts/Generator.cs
+++ Assets/Scripts/Generator.cs
@@ -6,21 +6,29 @@ public class Generator : MonoBehaviour
 {
     [SerializeField] private GameObject detectionZone;
     [SerializeField] private int life;
+    private Animator anim;
     
     private GameManager gameManager;
 
     void Start()
     {
+        anim = GetComponent<Animator>();
         gameManager = GameManager.gameManagerInstance;
         gameManager.generators++;
+        anim.Play("Idle");
     }
 
     public void PlayerHasInterracted()
     {
         life--;
+        if (life == 1)
+        {
+            anim.Play("Warning");
+        }
 
         if (life <= 0)
         {
+            anim.Play("Destroy");
             SoundManager.soundManagerInstance.PlaySound(SoundManager.SoundList.GENE_DESTROY, SoundManager.AudioMixerGroup.ENVIRONMENT);
             gameManager.generators--;
             Destroy(detectionZone);
