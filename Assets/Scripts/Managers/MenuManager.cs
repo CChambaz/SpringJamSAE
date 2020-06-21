54b9c89ebd2f47d353aa307bcab60e249cf71fcf
Sebastien Feser <sedukafe@gmail.com>
2020-06-21 14:50:27 +0200

    basic help menu

--- Assets/Scripts/Managers/MenuManager.cs
+++ Assets/Scripts/Managers/MenuManager.cs
@@ -5,6 +5,12 @@ using UnityEngine;
 public class MenuManager : MonoBehaviour
 {
     // Start is called before the first frame update
+
+    [SerializeField] private GameObject menu;
+    [SerializeField] private GameObject helpMenu;
+    //[SerializeField] private GameObject next;
+    //[SerializeField] private GameObject back;
+
     void Start()
     {
         
@@ -21,6 +27,29 @@ public class MenuManager : MonoBehaviour
         GameManager.gameManagerInstance.UpdateGameState(GameManager.GameState.GAME);
     }
 
+    public void Help()
+    {
+        menu.SetActive(false);
+        helpMenu.SetActive(true);
+    }
+
+    /*public void HelpNext(GameObject current, GameObject next)
+    {
+        next.SetActive(true);
+        back.SetActive(true);
+    }
+
+    public void HelpBack(GameObject current, GameObject back)
+    {
+        next.SetActive(true);
+        back.SetActive(true);
+    }*/
+
+    public void MainMenu()
+    {
+        helpMenu.SetActive(false);
+        menu.SetActive(true);
+    }
     public void ExitGame()
     {
         Application.Quit();
